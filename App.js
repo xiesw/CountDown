2c041017c1d8be1f014aaeb5ccbc191204155e71
pain.xie <pain.xie@wolaidai.com>
2018-03-16 11:52:16 +0800

    v1.0.0 提交图标

--- App.js
+++ App.js
@@ -14,13 +14,6 @@ import {
 import {RouterStack} from "./js/routers/index"
 import {Theme} from "./js/common/Theme";
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
 type Props = {};
 export default class App extends Component<Props> {
 
@@ -42,21 +35,3 @@ export default class App extends Component<Props> {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
--- js/scene/BaseScene.js
+++ js/scene/BaseScene.js
@@ -2,7 +2,6 @@
  * Created by pain.xie on 2018/3/8.
  */
 
-
 import React, {Component} from 'react';
 import {
   Text,
